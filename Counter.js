2ee4246b5cd9331b20e7f4561a22e5d7c99689d7
Boumedienne <cb_bouzidi@esi.dz>
2019-11-15 13:33:44 +0100

    Mastring node Js , relay Gtaphql Configuration

---
+++ Counter.js
@@ -0,0 +1,17 @@
+const EventEmitter=require('events').EventEmitter;
+const Counter=function(i){
+    this.increment=function(){
+        i++;
+        this.emit('increment',i);
+    }
+}
+
+Counter.prototype=new EventEmitter();
+const counter=new Counter(10);
+
+const callback=function(n){
+    console.log(n);
+}
+counter.addListener('incremened',callback);
+counter.increment(); 
+counter.increment(); 
--- Server.js
+++ Server.js
@@ -1,17 +1,19 @@
-var {graphql,buildSchema}=require('graphql');
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import {buildSchema} from 'graphql';
 
-var schema =buildSchema(`
-type Query {
-  hello:String
+const schema=buildSchema(`
+type Query{
+hello:String
 }
-
 `);
- var root   ={
-     hello:()=>{
-         return 'hello world';
-     }
- }
 
- graphql(schema,'{hello}',root).then((response)=>{
-   console.log(response);
- });
+const root ={hello:()=>'Hello world'};
+const app =express();
+app.use('/graphql',graphqlHTTP({
+  schema:schema,
+  rootValue:root,
+  graphiql:true,
+}))
+
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
